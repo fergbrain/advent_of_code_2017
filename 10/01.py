2ef020edb745cea85aa22367274cdcb512de567c
Andrew Ferguson <andrew@fergcorp.com>
2017-12-10 21:53:58 +0000

    cleaned up part 1

--- 10/01.py
+++ 10/01.py
@@ -1,7 +1,6 @@
 import csv
 
 puzInput = list(csv.reader(open('input', 'rb'), delimiter=','))
-
 #puzInput = [3, 4, 1, 5]
 puzInput = puzInput[0]
 
@@ -9,78 +8,32 @@ for a in range(0,len(puzInput)):
     puzInput[a] = int(puzInput[a])
     
 
-cir_list_max = 255 #255
-
-cir_list = []
-
-current_pos = 0
-
-skip = 0
-
-for i in range(0, cir_list_max+1):
-    cir_list.append(i)
-
-print "Cicular List: %s" % cir_list
+ln = 256 #255
+l = list(range(ln))
+p = 0
+s = 0
 
 for i in puzInput:
-    print "Length: %s" % i
-    print "Current Pos: %s" % current_pos
-    print "Skip: %s" % skip
-    print "Getting [%s,%s]" % (current_pos % (len(cir_list) ),( (current_pos+i-1) % len(cir_list)))
-    if ( current_pos % len(cir_list) ) >= ( (current_pos+i+1) % len(cir_list) ):
-        print "Wrapping"
-        temp_list = cir_list[current_pos % len(cir_list):] + cir_list[:(current_pos+i) % len(cir_list)] 
-        print "Temp: %s" % temp_list
+    if ( p % len(l) ) >= ( (p+i+1) % len(l) ):
+        temp_list = l[p % len(l):] + l[:(p+i) % len(l)] 
         temp_list = temp_list[::-1]
-        print "Temp (rev): %s" % temp_list
         
         k = 0
-        for j in range( (current_pos) % len(cir_list), len(cir_list)):
-            print "Putting %s into position %s" % (temp_list[k], j)
-            try:
-                cir_list[j] = temp_list[k]
-            except Exception as e:
-                print "j: %s | k: %s" % (j,k)
-                print e
-                exit()
+        for j in range( (p) % len(l), len(l)):
+            l[j] = temp_list[k]
             k += 1
-              
         for j in range(0, len(temp_list) - k  ):
-            print "Putting %s into position %s" % (temp_list[k], j)
-            try:
-                cir_list[j] = temp_list[k]
-            except Exception as e:
-                print "j: %s | k: %s" % (j,k)
-                print e
-                exit()
+            l[j] = temp_list[k]
             k += 1
-        
-        
     else:
-        temp_list = cir_list[current_pos % len(cir_list):(current_pos+i) % len(cir_list)]
-        print "Temp: %s" % temp_list
+        temp_list = l[p % len(l):(p+i) % len(l)]
         temp_list = temp_list[::-1]
-        #temp_list = cir_list[(current_pos+i-1) % len(cir_list) : (current_pos-1) % len(cir_list):-1]
-        print "Temp (rev): %s" % temp_list
         k = 0
-        for j in range(current_pos % len(cir_list), (current_pos+i) % len(cir_list)):
-            print "Putting %s into position %s" % (temp_list[k], j)
-            try:
-                cir_list[j] = temp_list[k]
-            except Exception as e:
-                print "j: %s | k: %s" % (j,k)
-                print e
-                exit()
+        for j in range(p % len(l), (p+i) % len(l)):
+            l[j] = temp_list[k]
             k += 1
-    
-
-        
-        
-        
-    print "Cicular List: %s" % cir_list
-    current_pos += i + skip
-    skip += 1
-    print ""
+    p += i + s
+    s += 1
 
 #print puzInput
-print cir_list[0] * cir_list[1]
+print l[0] * l[1]
